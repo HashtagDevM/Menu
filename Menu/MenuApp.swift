e1b341841932f6a8efc8dc81341f8449fd46f59f
MartinFlischman <mflischman@icloud.com>
2023-05-26 06:40:16 +0200

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Martin on 2023/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
