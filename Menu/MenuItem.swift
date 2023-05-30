df9e9fdb9559c869e802944fedeb02809d076a6d
MartinFlischman <mflischman@icloud.com>
2023-05-30 12:10:12 +0200

    Files added
    feat: menuItem struct added for the menuItems.
    
    feat: menuItems added with each one's name, price and image.

---
+++ Menu/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Martin on 2023/05/29.
+//
+
+import Foundation
+
+struct MenuItem {
+    var name: String
+    var price: String
+    var imageName: String
+}
